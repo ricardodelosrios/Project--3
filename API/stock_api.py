4b307ab20f2c45357647b82962ef28655df3a931
LM202345 <135395351+LM202345@users.noreply.github.com>
2023-09-24 16:49:54 -0400

    update API

--- API/stock_api.py
+++ API/stock_api.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 from flask import request
+from datetime import datetime
 
 import os
 from dotenv import load_dotenv
@@ -42,10 +43,11 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/tickers<br/>"
         f"/api/v1.0/stocks<br/>"
-        f"/api/v1.0/stocks?ticker=MSFT"
+        f"/api/v1.0/stocks?ticker=value<br/>"
+        f"/api/v1.0/volume?date=value<br/>"
     )
 
-
+# API to get all ticker
 @app.route("/api/v1.0/tickers")
 def names():
     session = Session(engine)
@@ -67,12 +69,12 @@ def names():
         session.close()
         return jsonify({"error": "Tickers not found"}), 404
 
-
+# API to get stocks by ticker or all stocks 
 @app.route("/api/v1.0/stock")
 def stock():
     session = Session(engine)
 
-    """Return a list with all stockes of selected ticker or all in no ticker selected"""
+    """Return a list with all stocks of selected ticker or all in no ticker selected"""
     ticker = request.args.get('ticker')
 
     if ticker:
@@ -100,7 +102,7 @@ def stock():
             session.close()
             return jsonify({"error": "Stocks for Ticker not found"}), 404
     else:
-        # If no symbol parameter is provided, return all ticker names
+        # If no ticker parameter is provided, return all ticker names
         results = session.query(Stock.date, Stock.ticker, Stock.open, Stock.high, Stock.low, Stock.close, Stock.volume, Stock.dividends, Stock.stocksplits).all()
 
         if results:
@@ -124,6 +126,44 @@ def stock():
             session.close()
             return jsonify({"error": "Stocks not found"}), 404
 
+#API to recover all stocs volume by selected date
+@app.route("/api/v1.0/volume")
+def volume():
+    session = Session(engine)
+
+    """Return a list with all stock volume of selected date"""
+    date = request.args.get('date')
+
+    if date:
+        try:
+            date_with_time = datetime.strptime(date, '%Y-%m-%d')
+            date_with_time = date_with_time.replace(hour=0, minute=0, second=0)
+        except ValueError:
+            session.close()
+            results = session.query(Stock.date, Stock.ticker, Stock.volume).filter_by(date='2023-01-03').all()
+            # return jsonify({"error": "Fecha en formato incorrecto"}), 400
+    else:
+        date_with_time = None
+
+    if date_with_time:
+        results = session.query(Stock.date, Stock.ticker, Stock.volume).filter_by(date=date_with_time).all()
+    else:
+        results = session.query(Stock.date, Stock.ticker, Stock.volume).all()
+
+    if results:
+        all_stocks = []
+        for date, ticker, volume in results:
+            stock_dict = {}
+            stock_dict["date"] = date
+            stock_dict["ticker"] = ticker
+            stock_dict["volume"] = volume
+            all_stocks.append(stock_dict)
+
+        session.close()
+        return jsonify(all_stocks)
+    else:
+        session.close()
+        return jsonify({"error": "Stocks not found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
