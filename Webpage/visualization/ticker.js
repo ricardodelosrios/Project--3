1340c3b979ec3dceb1ef1228397b50812eb73584
LM202345 <135395351+LM202345@users.noreply.github.com>
2023-09-24 16:51:01 -0400

    add js dropdown ticker

---
+++ Webpage/visualization/ticker.js
@@ -0,0 +1,32 @@
+function loadDropdown() {
+  var apiUrl = "http://127.0.0.1:5000/api/v1.0/tickers";
+
+  // Fetch data from the API using d3.json
+  return d3.json(apiUrl)
+    .then(function (data) {
+      var tickerDropdown = d3.select("#stock-select");
+
+      //data to dropdown options
+      tickerDropdown.selectAll("option")
+        .data(data)
+        .enter()
+        .append("option")
+        .attr("value", function (d) { return d.ticker; })
+        .text(function (d) { return d.name; });
+
+      return data;
+    });
+}
+
+// Call the loadDropdown function to populate the dropdown list
+loadDropdown()
+  .then(function (data) {
+    var selectedStock = document.getElementById('stock-select').value;
+    // Call functions to update charts
+    updateCandlestickChart(selectedStock);
+    updateBarChart(selectedStock);
+    updatePieChart(selectedStock, '2023-01-03');
+  })
+  .catch(function (error) {
+    console.error("Error:", error);
+  });
