b5f968ec231a25f9f0a4220a2b9c51532b3fb8b5
LM202345 <135395351+LM202345@users.noreply.github.com>
2023-09-25 20:03:47 -0400

    Update Widget / script

--- Webpage/scripts.js
+++ Webpage/scripts.js
@@ -15,49 +15,49 @@ new TradingView.widget({
     "container_id": "tv-chart-1"
 });
 
-// Function to update the Plotly candlestick chart based on the selected stock
-function updateCandlestickChart(selectedStock) {
-    // Load the merged data CSV
-    d3.csv('../Stock_data/Merged_historical_stock_data_daily.csv').then(function(data) {
-        // Filter data for the selected stock
-        const filteredData = data.filter(row => row.ticker === selectedStock);
+// // Function to update the Plotly candlestick chart based on the selected stock
+// function updateCandlestickChart(selectedStock) {
+//     // Load the merged data CSV
+//     d3.csv('../Stock_data/Merged_historical_stock_data_daily.csv').then(function(data) {
+//         // Filter data for the selected stock
+//         const filteredData = data.filter(row => row.ticker === selectedStock);
 
-        // Extract necessary columns
-        const dates = filteredData.map(row => row.date);
-        const openPrices = filteredData.map(row => parseFloat(row.open));
-        const highPrices = filteredData.map(row => parseFloat(row.high));
-        const lowPrices = filteredData.map(row => parseFloat(row.low));
-        const closePrices = filteredData.map(row => parseFloat(row.close));
+//         // Extract necessary columns
+//         const dates = filteredData.map(row => row.date);
+//         const openPrices = filteredData.map(row => parseFloat(row.open));
+//         const highPrices = filteredData.map(row => parseFloat(row.high));
+//         const lowPrices = filteredData.map(row => parseFloat(row.low));
+//         const closePrices = filteredData.map(row => parseFloat(row.close));
 
-        const trace = {
-            x: dates,
-            open: openPrices,
-            high: highPrices,
-            low: lowPrices,
-            close: closePrices,
-            type: 'candlestick',
-            name: 'Candlestick Chart'
-        };
+//         const trace = {
+//             x: dates,
+//             open: openPrices,
+//             high: highPrices,
+//             low: lowPrices,
+//             close: closePrices,
+//             type: 'candlestick',
+//             name: 'Candlestick Chart'
+//         };
 
-        const layout = {
-            title: `${selectedStock} Candlestick Chart`,
-            xaxis: { title: 'Date' },
-            yaxis: { title: 'Price' }
-        };
+//         const layout = {
+//             title: `${selectedStock} Candlestick Chart`,
+//             xaxis: { title: 'Date' },
+//             yaxis: { title: 'Price' }
+//         };
 
-        // Check if the chart container exists
-        if (document.getElementById('candlestick-chart')) {
-            // If it exists, update the existing chart
-            Plotly.react('candlestick-chart', [trace], layout);
-        }
-    });
-}
+//         // Check if the chart container exists
+//         if (document.getElementById('candlestick-chart')) {
+//             // If it exists, update the existing chart
+//             Plotly.react('candlestick-chart', [trace], layout);
+//         }
+//     });
+// }
 
-// Initialize the candlestick chart with the default stock (AAPL)
-updateCandlestickChart('AAPL');
+// // Initialize the candlestick chart with the default stock (AAPL)
+// updateCandlestickChart('AAPL');
 
-// Event listener for the stock selection dropdown
-document.getElementById('stock-select').addEventListener('change', function() {
-    const selectedStock = this.value;
-    updateCandlestickChart(selectedStock);
-});
+// // Event listener for the stock selection dropdown
+// document.getElementById('stock-select').addEventListener('change', function() {
+//     const selectedStock = this.value;
+//     updateCandlestickChart(selectedStock);
+// });
