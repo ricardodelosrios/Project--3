3699a2ee6323d8ae275b8a4c2f5f89fc1b865efc
Alex-WRK <6472421446a@gmail.com>
2023-09-27 19:39:10 -0400

    Data pull Code + SQL table creation

---
+++ Stock_data/SqlTableCreation.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS AAPL_data (
+    Date TIMESTAMP PRIMARY KEY,
+	Ticker VARCHAR(50),
+    Open FLOAT,
+    High FLOAT,
+    Low FLOAT,
+    Close FLOAT,
+    Volume INT,
+    Dividends FLOAT,
+    StockSplits FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS GOOGL_data (
+    Date TIMESTAMP PRIMARY KEY,
+	Ticker VARCHAR(50),
+    Open FLOAT,
+    High FLOAT,
+    Low FLOAT,
+    Close FLOAT,
+    Volume INT,
+    Dividends FLOAT,
+    StockSplits FLOAT
+);
